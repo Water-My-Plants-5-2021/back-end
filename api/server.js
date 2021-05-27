f63768c3b8d70706ef4f5ed6cd26c2b4bd2063cf
Allison Castaneda <allraec@yahoo.com>
2021-05-27 18:20:19 -0500

    heroku push

--- api/server.js
+++ api/server.js
@@ -8,11 +8,11 @@ const usersRouter = require("./users/users-router")
 const plantsRouter = require("./plants/plants-router")
 const db = require("./sample/sample-model")
 const config = {
-    origin: "http://localhost:3000",
-    credentials: true
+    origin: "*"
 };
-server.use(cors(config));
+
 server.use(helmet())
+server.use(cors(config));
 server.use(express.json())
 server.use(cookieParser())
 
