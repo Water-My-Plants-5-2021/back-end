00a64ae12400dc1df82c06aba506a2dee195cf57
Allison Castaneda <allraec@yahoo.com>
2021-05-26 20:15:46 -0500

    try

--- api/server.js
+++ api/server.js
@@ -8,8 +8,7 @@ const usersRouter = require("./users/users-router")
 const plantsRouter = require("./plants/plants-router")
 const db = require("./sample/sample-model")
 const config = {
-    origin: 'http://localhost:3000',
-    credentials: true
+    origin: "*"
 };
 server.use(cors(config));
 server.use(helmet())
